7896e69cedd20871b4211f9697f74c3dd0b28881
Cássia Cavalcante <rafaelsalgues0238@gmail.com>
2024-11-18 15:45:53 -0300

    projeto inicializado, configuração inicial do servidor

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import express, { Request, Response } from 'express'
+
+const app =express()
+
+app.use(express.json())
+
+app.get('/',(req:Request, res:Response):any=>{
+    return res.json('Tudo certo')
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log('Servidor Inicializado');
+    
+})
